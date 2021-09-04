0ed28cc3268dcd060947b354d17524a406e015ff
ivan-ramon-moran <ivan.ramon.moran@gmail.com>
2021-09-04 02:49:21 +0200

    v3

--- src/main/java/classes/HelloController.java
+++ src/main/java/classes/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Source To Image Example Working!!!";
+		return "Source To Image Example Working!!! v2";
 	}
 	
 	
